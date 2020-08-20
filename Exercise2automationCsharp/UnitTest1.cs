a0f52c3cb1ed635b78aa2cd61ceaa35e385135a2
Marisol Colon <marisol.colon@unosquare.com>
2020-08-20 11:24:13 -0500

    update packages

--- Exercise2automationCsharp/UnitTest1.cs
+++ Exercise2automationCsharp/UnitTest1.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Threading;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
